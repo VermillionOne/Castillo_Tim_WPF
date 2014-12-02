f6453ba1bc31659d299ec0280fa3e98df28ff2c0
November1112 <tjc0609@gmail.com>
2014-12-02 06:43:57 -0800

    Strings & Booleans
    Strings and booleans as well as backslash’s effect on characters.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,10 @@
-// JavaScript - Defining Variables
+// JavaScript
 
-var age; //declaration
-age = 38; //definition
-//storage container = value;
+var age = 38; //Number variable
 
-var yearOfBirth = 1970; //declaring and defining on the same line
+var isStudent = false; //Boolean variable
 
+var firstName = "Tim";
+
+var phrase = 'I don\'t know. \nYou\'ll have to speak with the professor';
+// "\" is able to give a character into an alternate use, for instance, making a special character (such as the single apostrophe) into a normal character, and using the letter "n" to create a linebreak.
