24f60d365c393268fc140fd973764bb4b0eb0cb9
November1112 <tjc0609@gmail.com>
2014-12-03 07:47:55 -0800

    Completed Example Expressions
    Completed Example Expressions with js doc.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,6 +1,6 @@
-// Expressions
+// Expressions - Age Example
 
-var a = 2; //sets up our variable "a" and defines it with "2"
-var b = a + 3;
-console.log(a);
-console.log(b);
+var yearBorn = 1988;
+var age = 2014 - yearBorn;
+
+console.log(age);
