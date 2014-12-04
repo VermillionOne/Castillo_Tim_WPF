47a528134c168200e7f52ba5d821a3a93fb82a06
November1112 <tjc0609@gmail.com>
2014-12-03 20:49:56 -0800

    Completed Arrays in Expressions
    Completed Arrays in Expressions using the scripts.js doc to create a
    simple array.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,16 +1,6 @@
-// Expressions - PEMDAS - The Order of Operations
+// Expressions - Expressions with Arrays
 
-var quiz1 = 86;
-var quiz2 = 79;
-var quiz3 = 100;
-var quiz4 = 95;
-var quiz5 = 91;
+var orangeBins = [234, 567, 883];
 
-//the average is the sum of the items divided by the number of items
-var average = (quiz1 + quiz2 + quiz3 + quiz4 + quiz5)/5;
-//console.log(average);
-
-var length = 7;
-var width = 6;
-var perimeter = length * 2 + width * 2;
-console.log(perimeter);
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
