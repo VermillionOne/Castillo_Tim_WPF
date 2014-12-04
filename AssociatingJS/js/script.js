e439bee8a0f89162fc0da6b46caad6a7b97b39fc
November1112 <tjc0609@gmail.com>
2014-12-03 20:13:30 -0800

    Completed Order of Operations
    Completed order of operations screencast with js doc

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,4 +1,16 @@
-// Expressions - Modulo Operator
+// Expressions - PEMDAS - The Order of Operations
 
-var remainder = 1989 % 2;
-console.log(remainder);
+var quiz1 = 86;
+var quiz2 = 79;
+var quiz3 = 100;
+var quiz4 = 95;
+var quiz5 = 91;
+
+//the average is the sum of the items divided by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4 + quiz5)/5;
+//console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
