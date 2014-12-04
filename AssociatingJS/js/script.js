1d1bd868e754e21d7442d30d377a6f41f23d2412
November1112 <tjc0609@gmail.com>
2014-12-03 20:56:41 -0800

    Completed Concatenating
    Completed Concatenating Strings by following along with the scripts.js
    doc.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,6 +1,13 @@
-// Expressions - Expressions with Arrays
+// Expressions - Concatenating Strings
 
-var orangeBins = [234, 567, 883];
+var firstName = "Timothy";
+var lastName = "Castillo";
+var fullName = firstName + " " + lastName
 
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-console.log(total);
+console.log(fullName);
+
+var a = 6;
+var b = 7;
+
+var result = a + b;
+console.log(result);
