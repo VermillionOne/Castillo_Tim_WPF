9a4f1697feafaefc78096f8f444327f5cb4d7941
November1112 <tjc0609@gmail.com>
2014-12-04 00:49:56 -0800

    EW - Completed Problem 1 and 2
    Completed Problem 1 and 2 on worksheet

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,2 +1,37 @@
-// Expressions Worksheet
+//**** Expressions Worksheet ****//
 
+
+/********************************/
+// Problem #1: Dog Years
+/********************************/
+
+// Sparky's age in human years
+var sparkyAge = 16;
+//Sparky's age in dog years
+var dogYears = sparkyAge * 7;
+// Result of finding out how old Sparky is in dog years compared to human
+var result = "Sparky is " + sparkyAge + " in human years which is " + dogYears + " in dog years.";
+// Displaying output in console
+console.log(result);
+
+/********************************/
+// Problem #2: Slice of Pie Part I
+/********************************/
+
+// Number of pizzas ordered
+var pizzas = 20;
+// Number of people at party
+var people = 45;
+// Number of slices per pizza
+var slicesPerPizza = 8;
+// Number of slices of pizza per person
+var slicesPerPerson = pizzas * slicesPerPizza / people;
+// Complete answer for how many slices of pizza per person
+var result2 = "Each person at the party ate " + slicesPerPerson + " slices of pizza.";
+console.log(result2);
+
+/********************************/
+// Problem #3: Slice of Pie Part II
+/********************************/
+
+//
