ea5beffc2de63b612a3040a9b216fdd228eb91c7
November1112 <tjc0609@gmail.com>
2014-12-04 20:40:04 -0800

    Added in "Givens"
    Added in "Givens" for Expressions_Wacky Assignment

--- Week 2/Expressions_Wacky/js/script.js
+++ Week 2/Expressions_Wacky/js/script.js
@@ -7,5 +7,19 @@
 
 //alert("Test");
 
+//Number of collectors
+var collectors = prompt("How many gold mines, elixir collectors, or dark elixir drills do you have?");
+//Individual production rate of collectors
+var productRate = prompt("How fast do they produce the material per hour?");
+//Individual maximum capacity of collectors
+var maxCapacity = prompt("How much can each collector hold?");
+//Rate that the user manually collects the material
+var userCollect = prompt("How many hours on average between collecting your materials?");
+//Cost of item or upgrade
+var itemCost = prompt("How much will it cost to upgrade or purchase your item?");
 //
+
+
+
+
 
