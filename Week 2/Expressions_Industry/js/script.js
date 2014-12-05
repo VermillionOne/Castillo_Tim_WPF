66c447c049d723d3f0432e9fe02d4cf6a895e5e2
November1112 <tjc0609@gmail.com>
2014-12-04 21:44:47 -0800

    Added Asterisk to arithmetic
    Line 27 in Expressions_Industry kept prompting an error in the console
    due to no asterisk to denote a multiplication step.

--- Week 2/Expressions_Industry/js/script.js
+++ Week 2/Expressions_Industry/js/script.js
@@ -10,31 +10,33 @@
 
 //Cost of item
 var itemCost = prompt("What is the purchasing cost of the item?");
+console.log(itemCost);
 //Sale price of item
 var salePrice = prompt("What is the sale price of the item?");
+console.log(salePrice);
 //What is the promotional discount?
-var discount = prompt("What is the promotional discount percentage?");
+var discountPercent = prompt("What is the promotional discount percentage?");
+console.log(discountPercent);
 //What is the sales tax?
 var salesTax = prompt("What is the sales tax?");
+console.log(salesTax);
 //Shipping and handling cost
 var shippingCost = prompt("What is the cost of the shipping?");
+console.log(shippingCost);
 //Discount and tax applied to sale price
-var discountPrice = salePrice - salePrice(discount/100);
+var discountPrice = salePrice - salePrice*(discountPercent / 100);
+console.log(discountPrice);
 //Tax applied to sale price
-var afterTax = salePrice + salePrice(salesTax/100);
+var afterTax = discountPrice + discountPrice*(salesTax / 100);
+console.log(afterTax);
 //Shipping and purchasing cost added together
 var totalCost = shippingCost + itemCost;
+console.log(totalCost);
 //Total Profit
-var totalProfit = salePrice - totalCost;
+var totalProfit = afterTax - totalCost;
+console.log(totalProfit);
 //Concatenating the result
-var result = ("With a purchasing cost of $" + itemCost + " and a sale price of $" + salePrice + " this item will bring in $" + totalProfit + " after a " + discount + "% discount and $" + shippingCost + " in shipping costs.");
+var result = ("With a purchasing cost of $" + itemCost + " and a sale price of $" + salePrice + " this item will bring in $" + totalProfit + " after a " + discountPercent + "% discount and $" + shippingCost + " in shipping costs.");
 //Displaying the result
 alert(result);
-console.log(itemCost);
-console.log(salePrice);
-console.log(discount);
-console.log(salesTax);
-console.log(shippingCost);
-console.log(totalCost);
-console.log(totalProfit);
 console.log(result);
--- Week 2/Expressions_Personal/js/script.js
+++ Week 2/Expressions_Personal/js/script.js
@@ -13,19 +13,19 @@ Expressions_Personal Week #2
 
 // Trash Bag Capacity
 var bagCapacity = prompt("Maximum weight your trash bag can hold in pounds:");
+console.log(bagCapacity);
 // Trash used daily
 var dailyTrash = prompt("Pounds of trash gone through daily:");
+console.log(dailyTrash);
 // Timeframe given
-var timeFrame = prompt("Time Frame in days");
+var timeFrame = prompt("Time Frame in days:");
+console.log(timeFrame);
 // Time to fill bag
 var filledBag = bagCapacity / dailyTrash;
+console.log(filledBag);
 // How many times does this happen in given time frame?
 var bagsPerTime = timeFrame / filledBag;
+console.log(bagsPerTime);
 // Displaying the result in a string
 var result = alert("With " + dailyTrash + " pounds of trash used daily, " + bagsPerTime + " bags of trash will be used over the course of " +timeFrame+" days.");
-console.log(bagCapacity);
-console.log(dailyTrash);
-console.log(timeFrame);
-console.log(filledBag);
-console.log(bagsPerTime);
 console.log(result);
