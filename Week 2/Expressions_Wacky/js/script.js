0dd4b98ff7308eb42fe005ba6df34c69ca713156
November1112 <tjc0609@gmail.com>
2014-12-05 00:52:20 -0800

    Corrections / Commented Out Lines
    Made a couple of syntax corrections; commented out any variables
    dealing with capacity or user collection.

--- Week 2/Expressions_Wacky/js/script.js
+++ Week 2/Expressions_Wacky/js/script.js
@@ -14,7 +14,7 @@ console.log(collectors);
 //Individual production rate of collectors
 var productRate = prompt("How fast do they produce the material per hour?");
 console.log(productRate);
-
+/*
 //Individual maximum capacity of collectors
 var capacity = prompt("How much can each collector hold?");
 console.log(capacity);
@@ -22,7 +22,7 @@ console.log(capacity);
 //Rate that the user manually collects the material
 var userCollect = prompt("How many hours on average between collecting your materials?");
 console.log(userCollect);
-
+*/
 //Name of Item or upgrade
 var itemName = prompt("What are you saving to buy or upgrade?");
 console.log(itemName);
@@ -33,12 +33,12 @@ console.log(itemCost);
 
 // Prompting the user to give current amount of material
 var currentAmount = prompt("How much material do you currently have?");
-console/log();
+console.log(currentAmount);
 
 //Total production rate
 var totalProdRate = collectors * productRate;
 console.log(totalProdRate);
-
+/*
 //Total Max Capacity
 var totalCapacity = capacity * collectors;
 console.log(totalCapacity);
@@ -46,15 +46,15 @@ console.log(totalCapacity);
 //Time for Total Max Capacity to fill
 var capProdTime = totalCapacity / totalProdRate;
 console.log(capProdTime);
-
+*/
 //Minimum Collection Time
-var minCollectTime = itemCost / capProdTime;
+var minCollectTime = itemCost / totalProdRate;
 console.log(minCollectTime);
-
+/*
 //Actual Collection Time
 var actualCollectTime = itemCost / (minCollectTime / userCollect)
 console.log(actualCollectTime);
-
+*/
 //Displaying Results in a string
 var result = ("You will be able to buy or upgrade the " + itemName + " in " + minCollectTime + " hours.");
 
