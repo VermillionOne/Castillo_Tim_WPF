307b02ab37b5cd30fb65cc6cf31502b5f6386e1f
November1112 <tjc0609@gmail.com>
2014-12-04 21:21:47 -0800

    Exp_Industry - Added console.log for each variable
    Added console.log for each variable in Expressions_Industry js doc

--- Week 2/Expressions_Industry/js/script.js
+++ Week 2/Expressions_Industry/js/script.js
@@ -19,7 +19,22 @@ var salesTax = prompt("What is the sales tax?");
 //Shipping and handling cost
 var shippingCost = prompt("What is the cost of the shipping?");
 //Discount and tax applied to sale price
-salePrice -= salePrice(discount/100) ;
+var discountPrice = salePrice - salePrice(discount/100);
 //Tax applied to sale price
-salePrice += salePrice(salesTax/100);
-//
+var afterTax = salePrice + salePrice(salesTax/100);
+//Shipping and purchasing cost added together
+var totalCost = shippingCost + itemCost;
+//Total Profit
+var totalProfit = salePrice - totalCost;
+//Concatenating the result
+var result = ("With a purchasing cost of $" + itemCost + " and a sale price of $" + salePrice + " this item will bring in $" + totalProfit + " after a " + discount + "% discount and $" + shippingCost + " in shipping costs.");
+//Displaying the result
+alert(result);
+console.log(itemCost);
+console.log(salePrice);
+console.log(discount);
+console.log(salesTax);
+console.log(shippingCost);
+console.log(totalCost);
+console.log(totalProfit);
+console.log(result);
