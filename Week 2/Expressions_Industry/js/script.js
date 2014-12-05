73d9324d5d94a19e13491faa3134e05d24a2f8e9
November1112 <tjc0609@gmail.com>
2014-12-04 20:51:08 -0800

    Completed given variables and prompts
    Completed given variables and prompts for Expressions_Industry

--- Week 2/Expressions_Industry/js/script.js
+++ Week 2/Expressions_Industry/js/script.js
@@ -13,7 +13,12 @@ var itemCost = prompt("What is the purchasing cost of the item?");
 //Sale price of item
 var salePrice = prompt("What is the sale price of the item?");
 //What is the promotional discount?
-var itemCost = prompt("What is the promotional discount percentage?");
+var discount = prompt("What is the promotional discount percentage?");
+//What is the sales tax?
+var salesTax = prompt("What is the sales tax?");
+//Shipping and handling cost
+var shippingCost = prompt("What is the cost of the shipping?");
 //
+
 
 
