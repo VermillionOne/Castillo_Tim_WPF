17aa21fc0fc5230b65887d11c7b98f6dca3cf1c7
November1112 <tjc0609@gmail.com>
2014-12-05 00:22:10 -0800

    Beginning to write result
    Beginning to write result for Expressions_Wacky

--- Week 2/Expressions_Wacky/js/script.js
+++ Week 2/Expressions_Wacky/js/script.js
@@ -10,15 +10,48 @@
 //Number of collectors
 var collectors = prompt("How many gold mines, elixir collectors, or dark elixir drills do you have?");
 console.log(collectors);
+
 //Individual production rate of collectors
 var productRate = prompt("How fast do they produce the material per hour?");
+console.log(productRate);
+
 //Individual maximum capacity of collectors
-var maxCapacity = prompt("How much can each collector hold?");
+var capacity = prompt("How much can each collector hold?");
+console.log(capacity);
+
 //Rate that the user manually collects the material
 var userCollect = prompt("How many hours on average between collecting your materials?");
+console.log(userCollect);
+
+//Name of Item or upgrade
+var itemName = prompt("What are you saving to buy or upgrade?");
+
 //Cost of item or upgrade
-var itemCost = prompt("How much will it cost to upgrade or purchase your item?");
-//
+var itemCost = prompt("How much will it cost to purchase or upgrade your item?");
+console.log(itemCost);
+
+//Total production rate
+var totalProdRate = collectors * productRate;
+console.log(totalProdRate);
+
+//Total Max Capacity
+var totalCapacity = capacity * collectors;
+console.log(totalCapacity);
+
+//Time for Total Max Capacity to fill
+var capProdTime = totalCapacity / totalProdRate;
+console.log(capProdTime);
+
+//Minimum Collection Time
+var minCollectTime = itemCost / capProdTime;
+console.log(minCollectTime);
+
+//Actual Collection Time
+var actualCollectTime = itemCost / (minCollectTime / userCollect)
+console.log(actualCollectTime);
+
+//Displaying Results in a string
+var result = ("You will be able to buy or upgrade ");
 
 
 
