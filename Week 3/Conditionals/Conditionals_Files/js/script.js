c2ed396a0e2d8796470ba0689cebd29b849cea8e
November1112 <tjc0609@gmail.com>
2014-12-06 11:07:53 -0800

    Completed With an Expression Tutorial
    Completed With an Expression Tutorial

--- Week 3/Conditionals/Conditionals_Files/js/script.js
+++ Week 3/Conditionals/Conditionals_Files/js/script.js
@@ -5,12 +5,13 @@ Conditional Logic Tutorial
 12-05-2014
 */
 
-var kidHeight = 50;
+var kidHeight = 47;
 var minHeight = 48;
+var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 
