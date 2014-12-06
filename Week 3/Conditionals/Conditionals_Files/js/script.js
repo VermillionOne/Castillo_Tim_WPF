2fcd6a5b64ffc097e32960aa1c5d677c4cacd743
November1112 <tjc0609@gmail.com>
2014-12-06 11:32:56 -0800

    Completed If Else Tutorial
    Completed If Else Tutorial

--- Week 3/Conditionals/Conditionals_Files/js/script.js
+++ Week 3/Conditionals/Conditionals_Files/js/script.js
@@ -11,8 +11,9 @@ var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
-
+}else{
+    console.log("Sorry, kid... you've got some growing to do first!")
 }
