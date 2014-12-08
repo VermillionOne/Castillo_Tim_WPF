29841a5f4ecef086c5d9b7d32d60a4f9cda19377
November1112 <tjc0609@gmail.com>
2014-12-07 20:27:39 -0800

    Ternary Operators Tutorial B
    Ternary operator assigned to variable

--- Week 3/Conditionals/Conditionals_Files/js/script.js
+++ Week 3/Conditionals/Conditionals_Files/js/script.js
@@ -4,13 +4,17 @@ SDI Section #03WDD
 Conditional Logic Tutorial
 */
 
-var gpa = 53;
+var age = 9;
+var book;
 
-//if the gpa is over the min 2.0 score, the student can graduate.
-/*if(gpa > 2.0){
-    console.log("You can graduate!");
+//if the child is under 10, they get Green Eggs and Ham, otherwise they get The time Machine
+/*
+if(age < 10){
+    book = "Green Eggs and Ham";
 }else{
-    console.log("GPA is too low!");
-}*/
+    book = "The Time Machine";
+}
+*/
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
 
-(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
