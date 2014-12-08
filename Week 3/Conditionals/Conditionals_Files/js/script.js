6486c8e8d67e243d5992074491f374494f25545d
November1112 <tjc0609@gmail.com>
2014-12-07 19:18:00 -0800

    Logical Operators Tutorial - "And" Operator
    Using the logical operator, “&&”, to combine conditions -
    Fix - Edited to match tutorial video

--- Week 3/Conditionals/Conditionals_Files/js/script.js
+++ Week 3/Conditionals/Conditionals_Files/js/script.js
@@ -7,10 +7,10 @@ Conditional Logic Tutorial
 
 var budget = 300;
 var iPhonePrice = 199.99;
-var wonLottery = true;
+var paycheck = 200;
 
 //if the price of the phone is less than our budget
-if(iPhonePrice < budget && wonLottery > 300){
+if(iPhonePrice < budget && paycheck > 300){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
