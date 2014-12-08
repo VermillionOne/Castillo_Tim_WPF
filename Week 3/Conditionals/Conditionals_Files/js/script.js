6ca49200d8a6e5a7f4b41813e2c0f9ddba4be1dc
November1112 <tjc0609@gmail.com>
2014-12-07 18:50:52 -0800

    Logical Operators Tutorial - "And" Operator
    Using the logical operator, “&&”, to combine conditions

--- Week 3/Conditionals/Conditionals_Files/js/script.js
+++ Week 3/Conditionals/Conditionals_Files/js/script.js
@@ -5,19 +5,13 @@ Conditional Logic Tutorial
 12-05-2014
 */
 
-var kidHeight = 47;
-var minHeight = 48;
-var wParentHeight = 45; //the height of the kid with the parent
+var budget = 300;
+var iPhonePrice = 199.99;
+var wonLottery = true;
 
-//if the child is old enough, print to the console "you can ride!"
-//if the kid is over 48 inches in height
-if(kidHeight > minHeight){
-    //You can ride
-    console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
-    //You can ride with a parent
-    console.log("You can ride, but only with a parent present.")
+//if the price of the phone is less than our budget
+if(iPhonePrice < budget && wonLottery > 300){
+    console.log("We can buy the phone!");
 }else{
-    //Sorry you have growing to do
-    console.log("Sorry, kid... you've got some growing to do first!")
+    console.log("No phone for you!");
 }
