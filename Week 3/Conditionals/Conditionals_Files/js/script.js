bd39fafe1ec803cd980f246d607a8b4a0bac1d90
November1112 <tjc0609@gmail.com>
2014-12-07 19:29:08 -0800

    Logical Operator, “||”(or), to combine conditions
    Logical Operator, “||”(or), to combine conditions to find out if either
    one are true.

--- Week 3/Conditionals/Conditionals_Files/js/script.js
+++ Week 3/Conditionals/Conditionals_Files/js/script.js
@@ -5,12 +5,12 @@ Conditional Logic Tutorial
 12-05-2014
 */
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less than our budget
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery === true){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
