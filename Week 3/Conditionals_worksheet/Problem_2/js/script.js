7a5d5a0a79a561d20aa8e6e542cf2630f5dbec2a
November1112 <tjc0609@gmail.com>
2014-12-09 21:17:43 -0800

    Corrected Notes in Week 3 Worksheet PR #2
    Corrected Notes in Week 3 Conditionals_worksheet Problem #2 - made
    notes for each expression

--- Week 3/Conditionals_worksheet/Problem_2/js/script.js
+++ Week 3/Conditionals_worksheet/Problem_2/js/script.js
@@ -17,15 +17,21 @@ console.log(username);
 var password = prompt("Enter Password");
 console.log(password);
 
+// Compare the entered username to the base username
 if(username != "Username"){
+    // If the Username doesn't equal the base username, give the message, "User not found. Please try again", as the result
     var result = "User not found. Please try again.";
     console.log(result);
+    // Compare the entered password to the base password
 }else if(password != "password"){
+    // If the password doesn't match, give the message, "Password does not match our records", as the result
     var result = "Password does not match our records.";
     console.log(result);
 }else{
+    // If both the password and username match, then give "Welcome, Tim!" as the result
     var result = "Welcome, Tim!";
     console.log(result);
 }
+// Print the result to the console
 alert(result);
 
