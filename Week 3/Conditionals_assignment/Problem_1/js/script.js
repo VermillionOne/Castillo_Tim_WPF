8140e75e2d680ef61b4960ec6e7219f7f320dd65
November1112 <tjc0609@gmail.com>
2014-12-10 07:30:14 -0800

    Setting Up Problem #1
    Setting up problem #1 - determining variables and values to use to best
    complete the problem.

--- Week 3/Conditionals_assignment/Problem_1/js/script.js
+++ Week 3/Conditionals_assignment/Problem_1/js/script.js
@@ -4,3 +4,20 @@
  * Conditionals_Assignment Problem 1 - Personal - Week #3
  * 12-10-2014
  */
+
+/*
+We are going on a road trip and we need to decide how many vehicles to take.
+We have four vehicles that we are able to use: Mitsubishi Outlander, Toyota Camry, Volkswagen Jetta, and Hyundai Entourage. Depending on the amount of people going, which vehicles will we need?
+    The amount of people each car can carry is as follows:
+        Hyundai Entourage: 7
+        Volkswagen Jetta: 5
+        Toyota Camry: 5
+        Mitsubishi Outlander: 5
+    We should also try to keep gas usage lower. In order to use the least amount of gasoline, we should use the cars with the highest Miles Per Gallon.
+        Hyundai Entourage: 18
+        Volkswagen Jetta: 25
+        Toyota Camry: 23
+        Mitsubishi Outlander: 20
+
+ */
+
