697ca0f2b8449bc7c11b0d55eff84ca0cf714fb0
November1112 <tjc0609@gmail.com>
2014-12-09 22:09:44 -0800

    Modified Conditionals_worksheet PR #1
    Modified Conditionals_worksheet Problem #1 to further specify conditions

--- Week 3/Conditionals_worksheet/Problem_1/js/script.js
+++ Week 3/Conditionals_worksheet/Problem_1/js/script.js
@@ -15,25 +15,30 @@ console.log(degrees);
 // Ask the user which scale the initial value is measured in.
 var scale = prompt("Is this temperature in Fahrenheit or Celsius?\n\nPlease give 'F' for Fahrenheit, or 'C' for Celsius.");
 console.log(scale);
-// Variable for holding conversion result
-var result;
 
 // Perform one conversion or the other based on whether Fahrenheit or Celsius were selected
 if(scale === "F" || scale === "f"){
+    // If the entered letter is exactly equal to "f" or "F", then perform the Fahrenheit to to Celsius conversion formula
     result = (degrees - 32) / 1.8;
     console.log(result);
-}else{
+}else if(scale === "C" || scale === "C"){
     result = degrees * 1.8 + 32;
     console.log(result);
-}
-
-//Display the results with the appropriate scale stated
-var displayResult;
-if(scale === "F" || scale === "f"){
-    alert("The temperature is " + result + " degrees Celsius.");
-    console.log("The temperature is " + result + " degrees Celsius.");
 }else{
-    alert("The temperature is " + result + " degrees Fahrenheit.");
-    console.log("The temperature is " + result + " degrees Fahrenheit.");
+    // If anything else is entered, display "Please try again."
+    alert("Please try again.");
 }
-console.log(displayResult);
+if(scale === "F" || scale === "f"){
+    // If the original scale is Fahrenheit, then display the answer showing the degrees in Celsius
+    var result = "The temperature is " + result + " degrees Celsius.";
+    console.log(result);
+    // Because it has already been found whether the letter "F" or "C" have been entered, it is not necessary to test further.
+}else if(scale === "C" || scale === "C"){
+    // If the original scale is Celsius, then display the answer showing the degrees in Fahrenheit
+    var result = "The temperature is " + result + " degrees Fahrenheit.";
+    console.log(result);
+}else{
+    alert("Refresh the page to use the converter.");
+}
+// Display Result
+alert(result);
