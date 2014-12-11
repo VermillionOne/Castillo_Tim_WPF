815cbecbc2c4f6ea00c275bb262b8c1f2400fd24
November1112 <tjc0609@gmail.com>
2014-12-10 23:53:17 -0800

    Began Week 2 Assignment Pr #2
    Began Week 2 Assignment Pr #2 - Added in variables and formulas

--- Week 3/Conditionals_assignment/Problem_2/js/script.js
+++ Week 3/Conditionals_assignment/Problem_2/js/script.js
@@ -4,3 +4,22 @@
  * Conditionals_Assignment Problem #2 - Industry - Week #3
  * 12-10-2014
  */
+
+
+var imageWidth = prompt("What is the width of the image in pixels?");
+
+var imageHeight = prompt("What is the Height of the image?");
+
+if ((imageWidth < 800 && imageWidth > 500) || (imageHeight < 800 && imageWidth > 500)){
+    "The image is the proper size."
+}else if(imageWidth > 800 || imageHeight > 800){
+    if(imageWidth > 800 && imageWidth > imageHeight){
+        var newImageWidth = "800";
+        var newImageHeight = imageHeight * newImageWidth / imageWidth;
+    }else{
+        var newImageHeight = "800";
+        var newImageWidth = imageWidth * newImageHeight / imageHeight;
+    }
+}else{
+    imageWidth=
+}
