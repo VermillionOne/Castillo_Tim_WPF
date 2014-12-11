f66c7488f7727131f945340412d447eac96b56fd
November1112 <tjc0609@gmail.com>
2014-12-10 22:46:30 -0800

    Began Conditionals_assignment PR #1
    Began Conditionals_assignment Problem #1 - added variables

--- Week 3/Conditionals_assignment/Problem_1/js/script.js
+++ Week 3/Conditionals_assignment/Problem_1/js/script.js
@@ -7,17 +7,23 @@
 
 /*
 We are going on a road trip and we need to decide how many vehicles to take.
-We have four vehicles that we are able to use: Mitsubishi Outlander, Toyota Camry, Volkswagen Jetta, and Hyundai Entourage. Depending on the amount of people going, which vehicles will we need?
-    The amount of people each car can carry is as follows:
-        Hyundai Entourage: 7
-        Volkswagen Jetta: 5
-        Toyota Camry: 5
-        Mitsubishi Outlander: 5
-    We should also try to keep gas usage lower. In order to use the least amount of gasoline, we should use the cars with the highest Miles Per Gallon.
-        Hyundai Entourage: 18
-        Volkswagen Jetta: 25
-        Toyota Camry: 23
-        Mitsubishi Outlander: 20
+We have four vehicles that we are able to use. Depending on the amount of people going, which vehicles will we need?
 
+    The amount of people each car can carry is as follows:
+
+    We should also try to keep gas usage lower. In order to use the least amount of gasoline, we should use the cars with the highest Miles Per Gallon.
  */
 
+var vehicle1 = prompt("What is the first vehicle?");
+var vehicle2 = prompt("What is the second vehicle?");
+var vehicle3 = prompt("What is the third vehicle?");
+var vehicle4 = prompt("What is the fourth vehicle?");
+
+var mileage1 = prompt("What is the mileage for " + vehicle1 + "?");
+var mileage2 = prompt("What is the mileage for " + vehicle2 + "?");
+var mileage3 = prompt("What is the mileage for " + vehicle3 + "?");
+var mileage4 = prompt("What is the mileage for " + vehicle4 + "?");
+
+var numPassengers = "How many people are going?";
+
+var numMiles = "How many miles long is the trip?";
