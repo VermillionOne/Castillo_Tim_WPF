fe9835edb04111b9d507909bc0d99ac0141ba8a6
November1112 <tjc0609@gmail.com>
2014-12-10 23:59:14 -0800

    Creating Result for Week 3 Problem #2
    Creating Result for Week 3 Problem #2 - NOTE previous commit was meant
    to say “Week 3” not “Week 2”

--- Week 3/Conditionals_assignment/Problem_2/js/script.js
+++ Week 3/Conditionals_assignment/Problem_2/js/script.js
@@ -10,10 +10,14 @@ var imageWidth = prompt("What is the width of the image in pixels?");
 
 var imageHeight = prompt("What is the Height of the image?");
 
-if ((imageWidth < 800 && imageWidth > 500) || (imageHeight < 800 && imageWidth > 500)){
+var minSize = 500;
+
+var maxSize = 800;
+
+if ((imageWidth < maxSize && imageWidth > 500) || (imageHeight < 800 && imageWidth > 500)){
     "The image is the proper size."
-}else if(imageWidth > 800 || imageHeight > 800){
-    if(imageWidth > 800 && imageWidth > imageHeight){
+}else if(imageWidth > maxSize || imageHeight > maxSize){
+    if(imageWidth > maxSize && imageWidth > imageHeight){
         var newImageWidth = "800";
         var newImageHeight = imageHeight * newImageWidth / imageWidth;
     }else{
@@ -21,5 +25,7 @@ if ((imageWidth < 800 && imageWidth > 500) || (imageHeight < 800 && imageWidth >
         var newImageWidth = imageWidth * newImageHeight / imageHeight;
     }
 }else{
-    imageWidth=
+    imageWidth = minSize;
 }
+
+var result = "The image has been adjusted to "
