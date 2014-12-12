818c4db92aaf0166092884f2eef259575c3371a9
November1112 <tjc0609@gmail.com>
2014-12-11 19:34:03 -0800

    Conditionals_Assignment Problem #2 - Start Rewrite
    Conditionals_Assignment Problem #2 - Start Rewrite

--- Week 3/Conditionals_assignment/Problem_2/js/script.js
+++ Week 3/Conditionals_assignment/Problem_2/js/script.js
@@ -5,6 +5,8 @@
  * 12-10-2014
  */
 
+// To determing whether an image needs resized
+
 // Ask the user for the width of the image in pixels
 var imageWidth = prompt("What is the width of the image in pixels?");
 console.log(imageWidth);
@@ -17,31 +19,5 @@ console.log(minSize);
 // Ask the user to set the maximum size
 var maxSize = prompt("What is the maximum size the longest side must be in pixels?");
 console.log(maxSize);
-// Creating a holder for new image width value
-//var newImageWidth;
 
-// Creating a holder for new image height value
-//var newImageHeight;
-// Compare the
-if (imageWidth > maxSize && imageWidth > imageHeight){
-    imageHeight = parseInt(imageHeight) * parseInt(maxSize) / parseInt(imageWidth);
-}else if(imageHeight > maxSize && imageHeight > imageWidth){
-    ImageWidth = parseInt(imageWidth) * parseInt(maxSize) / parseInt(imageHeight);
-}else if(imageWidth < minSize && imageHeight < minSize){
-    imageWidth = minSize;
-}else{
-    var rightSize = "The image is the proper size.";
-}
-
-endHeight = imageHeight.toFixed(0);
-endWidth = imageWidth.toFixed(0);
-// Create a results printout
-if(rightSize === "The image is the proper size."){
-    alert(rightSize);
-    console.log(rightSize)
-}else{
-    var result = "The image was adjusted to " + endWidth + "x" + endHeight + " resolution.";
-    alert(result);
-    console.log(result);
-}
 
