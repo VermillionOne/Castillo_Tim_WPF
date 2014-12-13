dfd309dc7662bb95809030bc8a23cc7ea19e922c
November1112 <tjc0609@gmail.com>
2014-12-12 19:10:12 -0800

    Completed Introduction to Functions Tutorial
    Completed Introduction to Functions Tutorial

---
+++ Week 4/Functions_tutorials/tuts_files/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Vermillion on 12/12/14.
+ */
+
+// Functions - Basic Structure
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
