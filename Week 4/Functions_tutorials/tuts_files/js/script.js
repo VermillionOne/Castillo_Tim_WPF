7b37941f44fc237128c9891f782d23e39afe929d
November1112 <tjc0609@gmail.com>
2014-12-15 19:56:59 -0800

    Completed Returning Values Tutorial
    Completed Returning Values Tutorial

--- Week 4/Functions_tutorials/tuts_files/js/script.js
+++ Week 4/Functions_tutorials/tuts_files/js/script.js
@@ -2,24 +2,15 @@
  * Created by Vermillion on 12/12/14.
  */
 
-// Functions - Parameters and Arguments
-
-/*
-calcArea(50, 20);
+// Functions - Returning Values
 
 
-function calcArea(w, h){ //w=30, h=20
+var total = calcArea(50, 20);
+
+
+function calcArea(w, h) { //w=30, h=20
     var area = w * h;
     console.log(area);
+    return area; //function spitting out the info
 }
-*/
-
-function dogYears(age){
-    var dogYears = age * 7;
-    console.log("Sparky is " + dogYears + " years old.")
-}
-
-var age1 = 7;
-
-dogYears(3);
-dogYears(6);
+console.log(total);
