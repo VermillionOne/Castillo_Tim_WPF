ec0f6a8c3c0d80d3a1b1c1786c715e0b1ec1bc65
November1112 <tjc0609@gmail.com>
2014-12-15 22:07:11 -0800

    Completed Do While Loops Tutorial

--- Week 4/Loops_tutorials/tuts_files/js/script.js
+++ Week 4/Loops_tutorials/tuts_files/js/script.js
@@ -4,9 +4,23 @@
 
 // Functions - While Loop
 
+
+console.log('-------------- Loops --------------')
+
 var b = 10;  //sets up the index
 
 while (b > 0){ // Checks the condition
     console.log(b + " kegs on the wall" );
     b--; //increments or decrements the index
 }
+
+console.log('-------------- Do While Loops --------------')
+
+
+var c = 10;
+
+do{
+    console.log(c + ' kegs on the wall');
+    c--;
+}
+while(c > 0);
