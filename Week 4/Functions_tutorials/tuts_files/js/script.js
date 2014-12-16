eccb376fd079893c513f6036030011c427d7dffa
November1112 <tjc0609@gmail.com>
2014-12-15 20:12:54 -0800

    Completed Anonymous Functions Tutorial
    Completed Anonymous Functions Tutorial

--- Week 4/Functions_tutorials/tuts_files/js/script.js
+++ Week 4/Functions_tutorials/tuts_files/js/script.js
@@ -2,23 +2,14 @@
  * Created by Vermillion on 12/12/14.
  */
 
-// Functions - Function vs. Procedure
+// Functions - Anonymous Functions
 
+var calcArea = function(width, height){ //defining
+    //code the function runs
+    var area = width * height;
+    return area;
+};
 
-var total = calcArea(50, 20);
+var a = calcArea(35, 20); //invoking
 
-// This is a function
-function calcArea(w, h) { //w=30, h=20
-    var area = w * h;
-    return area; // Functions return data
-}
-
-// This is a procedure
-function calcArea2(w, h) { //w=30, h=20
-    var area = w * h;
-    console.log(area); //Procedures only provide instruction
-}
-
-calcArea2(45,3);
-
-console.log(total);
+console.log(a);
