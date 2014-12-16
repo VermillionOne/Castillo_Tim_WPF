185a98311c12e26372c4dcf62335510a2704a458
November1112 <tjc0609@gmail.com>
2014-12-15 20:06:50 -0800

    Completed Functions vs Procedures Tutorial
    Completed Functions vs Procedures Tutorial

--- Week 4/Functions_tutorials/tuts_files/js/script.js
+++ Week 4/Functions_tutorials/tuts_files/js/script.js
@@ -2,15 +2,23 @@
  * Created by Vermillion on 12/12/14.
  */
 
-// Functions - Returning Values
+// Functions - Function vs. Procedure
 
 
 var total = calcArea(50, 20);
 
-
+// This is a function
 function calcArea(w, h) { //w=30, h=20
     var area = w * h;
-    console.log(area);
-    return area; //function spitting out the info
+    return area; // Functions return data
 }
+
+// This is a procedure
+function calcArea2(w, h) { //w=30, h=20
+    var area = w * h;
+    console.log(area); //Procedures only provide instruction
+}
+
+calcArea2(45,3);
+
 console.log(total);
