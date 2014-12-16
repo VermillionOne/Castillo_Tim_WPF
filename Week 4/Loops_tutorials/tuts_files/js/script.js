0168ca3838f32cf07c6ca52af7092f8b1a604864
November1112 <tjc0609@gmail.com>
2014-12-15 21:59:03 -0800

    Completed While Loop Tutorial
    Completed While Loop Tutorial

---
+++ Week 4/Loops_tutorials/tuts_files/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Vermillion on 12/14/14.
+ */
+
+// Functions - While Loop
+
+var b = 10;  //sets up the index
+
+while (b > 0){ // Checks the condition
+    console.log(b + " kegs on the wall" );
+    b--; //increments or decrements the index
+}
