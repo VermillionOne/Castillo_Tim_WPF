bdeb1ee1ab0c0865d36688e249e1c9296e9904a4
November1112 <tjc0609@gmail.com>
2014-12-17 05:09:55 -0800

    Completed Stung! Functions_worksheet
    Completed Stung! Functions_worksheet

--- Week 4/Functions_worksheet/js/script.js
+++ Week 4/Functions_worksheet/js/script.js
@@ -12,8 +12,12 @@ function circle(radius){
 	var calcCirc = 2 * radius * 3.14;  // Circumference formula
 	return calcCirc;  // Returning the circumference value
 };
-// Invoking the function with a user input
-var result = circle(prompt("What is the radius of the circle?"));
+// Get the radius of the circle from the user
+var radius = prompt("What is the radius of the circle?");
+// Invoking the function with a user input and gather the returned result
+var circumference = circle(radius);
+// Concatenate the results into a string
+var result = "The circumference of a circle with a radius of " + radius + " is " + circumference + ".";
 // Printing the result to the console
 console.log(result);
 // Displaying the result to the user
@@ -21,3 +25,18 @@ alert(result);
 
 /*------- Finding the Fatal Amount of Bee Stings per Animal -------*/
 
+// Creating a function using the bee sting to weight ratio to find out how many stings it would take to kill an animal
+function beeSting(weight){
+	var ratio = weight / 8.666666667;
+	return ratio;
+}
+
+// Get the weight of the animal from the user
+var weight = prompt("How much does the animal weigh in pounds?");
+// Invoke the function and gather the returned result
+var fatalStings = beeSting(weight);
+// Concatenate the results into a string.
+var result2 = "It takes " + fatalStings + " bee stings to kill an animal weighing " + weight + " pounds.";
+
+console.log(result2);
+alert(result2);
