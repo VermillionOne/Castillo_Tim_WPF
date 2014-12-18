c3b3a7d34566552e0ddf2e7697d85d8629ab1313
November1112 <tjc0609@gmail.com>
2014-12-18 04:44:24 -0800

    Functions_assignment Industry
    Working out bugs. Result is displaying too large a number

--- Week 4/Functions_assignment/Functions_industry/js/script.js
+++ Week 4/Functions_assignment/Functions_industry/js/script.js
@@ -4,8 +4,6 @@
  * Functions_industry Week #4
  * 12-17-2014
 */
-// Invoking the Lateral Surface area function
-var surfaceArea = lateralSurface(radius, height);
 
 // Asking the user for the radius of the Christmas Tree
 var radius = prompt("What is the radius of the Christmas tree in feet?");
@@ -16,13 +14,15 @@ var height = prompt("What is the height of the Christmas Tree in feet?");
 console.log(height);
 
 // Asking the user for the size of the ornaments
-var ornamentSize = prompt("How big are the Christmas ornaments?\n\nPlease provide us the diameter of the ornament.");
+var ornamentSize = prompt("How big are the Christmas ornaments?\n\nPlease provide us the diameter of the ornaments in inches.");
 console.log(ornamentSize);
+
+// Invoking the Lateral Surface area function
+var surfaceArea = lateralSurface(radius, height);
 
 // Invoking the function for the number of ornaments
 var ornamentNumber = ornaments(surfaceArea, ornamentSize);
 
-var result = "It will take " + ornamentNumber + " ornaments to fully dress the Christmas Tree.";
 
 // This function is for finding the lateral surface area of the cone
 function lateralSurface(r, h){
@@ -40,8 +40,15 @@ function lateralSurface(r, h){
 
 // Function for finding out the number of ornaments
 function ornaments(area, size){
-	var ornamentNumber = area / (size + 2) * (size + 2);
-	console.log(ornamentNumber);
-	return ornamentNumber;
+	var areaInches =  area * 144;
+	console.log(areaInches);
+	var amount = areaInches / (size + 2) * (size + 2);
+	console.log(amount);
+	return amount;
 }
+// Concatenate the result string
+var result = "It will take " + ornamentNumber + " ornaments to fully dress the Christmas Tree.";
+// Display the result
+alert(result);
+console.log(result);
 
