cfeba5befd8cb963158964dfbd68dc12fbfedde8
November1112 <tjc0609@gmail.com>
2014-12-18 20:19:12 -0800

    Began Functions_industry assignment
    Began Functions_industry assignment

--- Week 4/Functions_assignment/Functions_industry/js/script.js
+++ Week 4/Functions_assignment/Functions_industry/js/script.js
@@ -5,4 +5,18 @@
  * 12-17-2014
 */
 
+// A quick survey for the user.
 
+var name = prompt("Hello! What is your name?");
+
+var survey = prompt("Thank you for visting our site,  " + name + "! We hope you enjoyed the experience.\n\nWould you be willing to take a quick survey?");
+
+survey = str.toLowerCase(survey);
+
+answer(survey);
+
+function answer(i){
+    if(i != )
+}
+
+if ()
