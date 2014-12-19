71829295ddf2f2bd033b4fa88fbee6393bb27ea0
November1112 <tjc0609@gmail.com>
2014-12-18 22:39:31 -0800

    Functions_personal added variables and functions
    Added variables and ternary and began work on anonymous function

--- Week 4/Functions_assignment/Functions_personal/js/script.js
+++ Week 4/Functions_assignment/Functions_personal/js/script.js
@@ -5,18 +5,20 @@
  * 12-17-2014
 */
 
-// Define types of fish in an array and ask how much of each does the user want
-var fish = new Array() ;
-    fish[0] = prompt("How many Goldfish do you want?");
-    fish[1] = prompt("How many Tetras do you want?");
-    fish[2] = prompt("How many Loaches do you want?");
-    fish[3] = prompt("How many Cherry Barbs do you want?");
-    fish[4] = prompt("How many Mollies do you want?");
 
-var gallons = function(a,b,c,d,e,f){
+// Will the item ship out today?
+
+var shipment = function(hour){
 
 }
 
+var hour = prompt("In what hour was the product ordered");
 
+var dayNight = prompt("Was it ordered during am or pm?");
 
+var nightDay = str.toLowerCase(dayNight);
 
+timeOfOrder = (nightDay === "pm" ) ? parseInt(hour) + 12 : parseInt(hour);
+console.log(timeOfOrder)
+
+var result = shipment(timeOfOrder);
