76c5d724350433ab879f0a7e4e8e128f8bfb51c1
November1112 <tjc0609@gmail.com>
2014-12-18 23:55:46 -0800

    Specific verification in Functions_industry
    Attempting specific verification in Functions_industry unsuccessfully

--- Week 4/Functions_assignment/Functions_industry/js/script.js
+++ Week 4/Functions_assignment/Functions_industry/js/script.js
@@ -18,12 +18,38 @@ while (isNaN(year) || year === ""){
 console.log(year);
 // Ask for birth month
 var month = prompt("In which month were you born?");
+
 //Create a loop to verify the number for the month
-while (isNaN(month) || month === ""){
+while ((month === "") || (month != "january") || (month != "february") || (month != "march") || (month != "april") || (month != "may") || (month != "june") || (month != "july") || (month != "august") || (month != "september") || (month != "october") || (month != "november") || (month != "december")){
     //Prompt again for month
-    month = prompt("Please enter the month you were born.\n\nPlease enter a number only.");
+    month = prompt("Please enter the month you were born.\n\nPlease enter a number or name of the month.");
 }
-console.log(month);
+if (month = "january"){
+    var month = 1;
+}else if(month === "february"){
+    var month = 2;
+}else if(month === "march"){
+    var month = 3;
+}else if(month === "april"){
+    var month = 4;
+}else if(month === "may"){
+    var month = 5;
+}else if(month === "june"){
+    var month = 6;
+}else if(month === "july"){
+    var month = 7;
+}else if(month === "august"){
+    var month =8 ;
+}else if(month === "september"){
+    var month = 9;
+}else if(month === "october"){
+    var month = 10;
+}else if(month === "november"){
+    var month = 11;
+}else{
+    var month = 12;
+}
+console.log(monthnew);
 // Ask for day of birth
 var day = prompt("What day of the month were you born?");
 //Create a loop to verify the number for the day
