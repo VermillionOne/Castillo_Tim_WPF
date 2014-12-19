fac3cb57a64e5fe77cfa857adc5fac323c26576f
November1112 <tjc0609@gmail.com>
2014-12-18 20:55:36 -0800

    Function_industry Finished prompts
    Function_industry Finished prompts - Completed writing prompts

--- Week 4/Functions_assignment/Functions_industry/js/script.js
+++ Week 4/Functions_assignment/Functions_industry/js/script.js
@@ -10,20 +10,25 @@
 
 // Ask for year born in
 var year = prompt("In what year were you born?");
-if (year = NaN){
-
+while (isNaN(year) || year === ""){
+    year prompt("Please enter the year you were born.\n\nPlease enter a number only");
 }
 console.log(year);
 // Ask for birth month
 var month = prompt("In which month were you born?");
+while (isNaN(month) || month === ""){
+    month = prompt("Please enter the month you were born.\n\nPlease enter a number only");
+}
 console.log(month);
 // Ask for day of birth
 var day = prompt("What day of the month were you born?");
+while (isNaN(day) || day === ""){
+    day = prompt("Please enter the day of the month you were born.\n\nPlease enter a number only");
+}
 console.log(day);
 
+birthday(month, day, year);
 
-
-
-var birthday = function(m, d, y){
-
+function birthday(m, d, y){
+    return = m + " / " + d + " / " + y;
 }
