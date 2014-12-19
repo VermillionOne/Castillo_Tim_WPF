861c9328f6e8404fcff4a89a79f2e7b211a80efa
November1112 <tjc0609@gmail.com>
2014-12-18 23:31:16 -0800

    Functions_personal Comments added
    Functions_personal Comments added

--- Week 4/Functions_assignment/Functions_personal/js/script.js
+++ Week 4/Functions_assignment/Functions_personal/js/script.js
@@ -2,7 +2,7 @@
  * Tim Castillo
  * SDI Section #03WDD
  * Functions_personal Week #4
- * 12-17-2014
+ * 12-18-2014
 */
 
 
@@ -24,18 +24,20 @@ var shipment = function(hour){
 // Find out what hour the item was ordered
 var hour = prompt("During what hour was the product ordered?");
 //Create a loop to verify the number for the hour
-while ((isNaN(hour) || hour === "") && (hour <= 0 && hour > 24 )){
+while ((isNaN(hour) || hour === "") && (hour <= 0 && hour > 12)){
     //Prompt again to get hour
-    hour = prompt("What hour was the product ordered?\n\nPlease enter a number 1 and 12 only");
-var dayNight = prompt("Was it ordered during am or pm?");
-    
+    hour = prompt("What hour was the product ordered?\n\nPlease enter a number between 1 and 12 only");
+}
+// Find out if the hour was AM or PM
+var dayNight = prompt("Was it ordered during AM or PM?");
+// Force the string for AM or PM to lower case to rule out differentiation
 var nightDay = dayNight.toLowerCase();
-
-timeOfOrder = (nightDay === "pm" ) ? parseInt(hour) + 12 : parseInt(hour);
+// Adjust the hour according to whether it was during AM or PM by adding 12 if it was during PM
+timeOfOrder = (nightDay === "pm") ? parseInt(hour) + 12 : parseInt(hour);
 console.log(timeOfOrder);
-
+// Invoke the function to determine when the item will ship out
 var result = shipment(timeOfOrder);
-
+// Display the shipment results
 alert(result);
 console.log(result);
 
