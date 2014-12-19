58f371fbdd8a508321750cd068ea76c93b67f4bc
November1112 <tjc0609@gmail.com>
2014-12-18 21:43:01 -0800

    Added Comments to Functions_Industry
    Finished adding comments to clearly describe each line

--- Week 4/Functions_assignment/Functions_industry/js/script.js
+++ Week 4/Functions_assignment/Functions_industry/js/script.js
@@ -8,7 +8,7 @@
 
 // Ask the user for date of birth
 
-// Ask for year born in
+// Ask for year in which born
 var year = prompt("In what year were you born?");
 //Create a loop to verify the number for the year
 while (isNaN(year) || year === ""){
@@ -26,17 +26,22 @@ while (isNaN(month) || month === ""){
 console.log(month);
 // Ask for day of birth
 var day = prompt("What day of the month were you born?");
+//Create a loop to verify the number for the day
 while (isNaN(day) || day === ""){
+    //prompy for day again
     day = prompt("Please enter the day of the month you were born.\n\nPlease enter a number only");
 }
 console.log(day);
 
-function birthday(m, d, y){
-    var date = "Your birthday is " + m + "/" + d + "/" + y + ".";
-    return(date);
-}
-
+// Invoke the function to create the result
 var result = birthday(month, day, year);
 
+// Create a function to put together the date of birth
+function birthday(m, d, y){
+    // Concatenating the parts in a string
+    var date = "Your birthday is " + m + "/" + d + "/" + y + ".";
+    return(date); // return the value to the result
+}
+// Display the result
 alert(result);
 console.log(result);
