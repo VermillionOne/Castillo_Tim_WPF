d0c5eb6b68a1a6ea2a2617512ca718cddb91e940
November1112 <tjc0609@gmail.com>
2014-12-19 00:01:43 -0800

    Reverting Functions_industry to previous code
    Reverting Functions_industry to previous code

--- Week 4/Functions_assignment/Functions_industry/js/script.js
+++ Week 4/Functions_assignment/Functions_industry/js/script.js
@@ -3,7 +3,7 @@
  * SDI Section #03WDD
  * Functions_industry Week #4
  * 12-17-2014
-*/
+ */
 
 
 // Ask the user for date of birth
@@ -18,49 +18,24 @@ while (isNaN(year) || year === ""){
 console.log(year);
 // Ask for birth month
 var month = prompt("In which month were you born?");
-
 //Create a loop to verify the number for the month
-while ((month === "") || (month != "january") || (month != "february") || (month != "march") || (month != "april") || (month != "may") || (month != "june") || (month != "july") || (month != "august") || (month != "september") || (month != "october") || (month != "november") || (month != "december")){
+while (isNaN(month) || month === ""){
     //Prompt again for month
-    month = prompt("Please enter the month you were born.\n\nPlease enter a number or name of the month.");
+    month = prompt("Please enter the month you were born.\n\nPlease enter a number only.");
 }
-if (month = "january"){
-    var month = 1;
-}else if(month === "february"){
-    var month = 2;
-}else if(month === "march"){
-    var month = 3;
-}else if(month === "april"){
-    var month = 4;
-}else if(month === "may"){
-    var month = 5;
-}else if(month === "june"){
-    var month = 6;
-}else if(month === "july"){
-    var month = 7;
-}else if(month === "august"){
-    var month =8 ;
-}else if(month === "september"){
-    var month = 9;
-}else if(month === "october"){
-    var month = 10;
-}else if(month === "november"){
-    var month = 11;
-}else{
-    var month = 12;
-}
-console.log(monthnew);
+console.log(month);
 // Ask for day of birth
 var day = prompt("What day of the month were you born?");
 //Create a loop to verify the number for the day
 while (isNaN(day) || day === ""){
-    //prompt for day again
+    //prompy for day again
     day = prompt("Please enter the day of the month you were born.\n\nPlease enter a number only");
 }
 console.log(day);
 
 // Invoke the function to create the result
 var result = birthday(month, day, year);
+
 // Create a function to put together the date of birth
 function birthday(m, d, y){
     // Concatenating the parts in a string
