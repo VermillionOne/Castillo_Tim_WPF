557261b36e472c9672bb42674e60a6cccc650118
November1112 <tjc0609@gmail.com>
2014-12-18 22:46:31 -0800

    Functions_personal else if added
    finished else if argument in function

--- Week 4/Functions_assignment/Functions_personal/js/script.js
+++ Week 4/Functions_assignment/Functions_personal/js/script.js
@@ -9,7 +9,11 @@
 // Will the item ship out today?
 
 var shipment = function(hour){
-
+    if(hour > 0 && hour < 15){
+        var ship = "The item will ship today"
+    } else {
+        var ship = "The item will ship tomorrow"
+    }
 }
 
 var hour = prompt("In what hour was the product ordered");
@@ -19,6 +23,6 @@ var dayNight = prompt("Was it ordered during am or pm?");
 var nightDay = str.toLowerCase(dayNight);
 
 timeOfOrder = (nightDay === "pm" ) ? parseInt(hour) + 12 : parseInt(hour);
-console.log(timeOfOrder)
+console.log(timeOfOrder);
 
 var result = shipment(timeOfOrder);
