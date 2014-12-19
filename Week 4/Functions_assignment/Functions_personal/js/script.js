875e173f40feecbf96ed0c97ce99bc3a1901c97f
November1112 <tjc0609@gmail.com>
2014-12-18 22:06:58 -0800

    Added New Array to Functions_personal
    Added New Array to Functions_personal

--- Week 4/Functions_assignment/Functions_personal/js/script.js
+++ Week 4/Functions_assignment/Functions_personal/js/script.js
@@ -5,5 +5,18 @@
  * 12-17-2014
 */
 
+// Define types of fish in an array and ask how much of each does the user want
+var fish = new Array() ;
+    fish[0] = prompt("How many Goldfish do you want?");
+    fish[1] = prompt("How many Tetras do you want?");
+    fish[2] = prompt("How many Loaches do you want?");
+    fish[3] = prompt("How many Cherry Barbs do you want?");
+    fish[4] = prompt("How many Mollies do you want?");
 
-//
+var gallons = function(a,b,c,d,e,f){
+
+}
+
+
+
+
